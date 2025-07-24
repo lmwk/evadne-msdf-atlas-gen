2b5fce19d8601228725694c0fe11d2027bceb34a
lmwk <47907204+lmwk@users.noreply.github.com>
2025-07-24 10:19:06 -0500

    Updating msdfgen and adding back temp workspace to build

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,19 @@
+workspace "MSDF-Temp"
+	architecture "x64"
+	
+	configurations 
+	{ 
+		"Debug", 
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "msdfgen"
 
